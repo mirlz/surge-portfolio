e4f904bb506ff19a803b7627034dd14eea85e45a
DESKTOP-MUMGF1G\mirlz <mirlzy@gmail.com>
2018-12-05 23:15:59 +0800

    feat: update logo fix: typo error

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import logopic from '../images/favicon.png'
 
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-code"></span>
+            {/* <span className="icon fa-code"></span> */}
+            <img className="logopic" src={logopic} alt="" title="MirlZ" />
         </div>
         <div className="content">
             <div className="inner">
                 <h1>Front-end Web Development</h1>
                 <p>Hello, my name is Eugenia. I am a front-end web developer who's based in Singapore. I am motivated in achieving good visual aesthetics for websites and making it easy to use. </p> 
-                <p>I'm currently working on React / JS web applications. I have recently joined <a href="https://www.zenika.sg/#/" target="_blank" title="Zenika Singapores">Zenika Singapore</a> as a consultant. Feel free to contact me for any interesting projects! ;) </p>
+                <p>I'm currently working on ReactJS / MobX web applications. I have recently joined <a href="https://www.zenika.sg/#/" target="_blank" title="Zenika Singapores">Zenika Singapore</a> as a consultant. Feel free to contact me for any interesting projects! ;) </p>
             </div>
         </div>
         <nav>
